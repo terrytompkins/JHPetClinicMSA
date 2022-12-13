9e93106d69e064dcb28228345e08bc4fe55340c4
Terry Tompkins <tompkins@transwebtools.com>
2022-12-13 11:36:02 -0500

    Initial version of vet generated by generator-jhipster@7.9.3

---
+++ vet/src/main/java/com/jhipster/demo/vet/service/VisitService.java
@@ -0,0 +1,58 @@
+package com.jhipster.demo.vet.service;
+
+import com.jhipster.demo.vet.domain.Visit;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+/**
+ * Service Interface for managing {@link Visit}.
+ */
+public interface VisitService {
+    /**
+     * Save a visit.
+     *
+     * @param visit the entity to save.
+     * @return the persisted entity.
+     */
+    Visit save(Visit visit);
+
+    /**
+     * Updates a visit.
+     *
+     * @param visit the entity to update.
+     * @return the persisted entity.
+     */
+    Visit update(Visit visit);
+
+    /**
+     * Partially updates a visit.
+     *
+     * @param visit the entity to update partially.
+     * @return the persisted entity.
+     */
+    Optional<Visit> partialUpdate(Visit visit);
+
+    /**
+     * Get all the visits.
+     *
+     * @param pageable the pagination information.
+     * @return the list of entities.
+     */
+    Page<Visit> findAll(Pageable pageable);
+
+    /**
+     * Get the "id" visit.
+     *
+     * @param id the id of the entity.
+     * @return the entity.
+     */
+    Optional<Visit> findOne(Long id);
+
+    /**
+     * Delete the "id" visit.
+     *
+     * @param id the id of the entity.
+     */
+    void delete(Long id);
+}
