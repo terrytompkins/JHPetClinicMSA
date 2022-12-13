1c94baa57e0b18a5b5b1fb4bf8dd7d5523fa438c
Terry Tompkins <tompkins@transwebtools.com>
2022-12-13 12:08:26 -0500

    resetting application

--- visits/src/main/webapp/app/shared/model/pet/specialty.model.ts
+++
@@ -1,9 +0,0 @@
-import { IVet } from 'app/shared/model/pet/vet.model';
-
-export interface ISpecialty {
-  id?: number;
-  name?: string;
-  vets?: IVet[] | null;
-}
-
-export const defaultValue: Readonly<ISpecialty> = {};
--- visits/src/main/webapp/app/shared/model/pet/species.model.ts
+++
@@ -1,6 +0,0 @@
-export interface ISpecies {
-  id?: number;
-  name?: string;
-}
-
-export const defaultValue: Readonly<ISpecies> = {};
