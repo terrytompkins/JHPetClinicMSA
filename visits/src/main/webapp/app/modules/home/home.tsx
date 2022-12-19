635bc3bb6a70d07f16049f916635f997f421160f
Attila DeLisle <attila-delisle@idexx.com>
2022-12-19 12:15:03 -0500

    minor changes

--- visits/src/main/webapp/app/modules/home/home.tsx
+++ visits/src/main/webapp/app/modules/home/home.tsx
@@ -18,41 +18,38 @@ export const Home = () => {
       <Helmet>
         <title>PetClinic - Home</title>
       </Helmet>
-    <Row>
-      <Col md="12">
-        <h2>
-          <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
-        </h2>
-        <p className="lead">
-          <Translate contentKey="home.subtitle">This is your homepage</Translate>
-        </p>
-        {account?.login ? (
-          <Schedule />
-        ) : (
-          <div>
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
+      <Row>
+        <Col md="12">
+          <h2>
+            <Translate contentKey="home.title">Welcome, Java Hipster!</Translate>
+          </h2>
+          {account?.login ? (
+            <Schedule />
+          ) : (
+            <div>
+              <Alert color="warning">
+                <Translate contentKey="global.messages.info.authenticated.prefix">If you want to </Translate>
 
-              <Link to="/login" className="alert-link">
-                <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
-              </Link>
-              <Translate contentKey="global.messages.info.authenticated.suffix">
-                , you can try the default accounts:
-                <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
-                <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
-              </Translate>
-            </Alert>
+                <Link to="/login" className="alert-link">
+                  <Translate contentKey="global.messages.info.authenticated.link"> sign in</Translate>
+                </Link>
+                <Translate contentKey="global.messages.info.authenticated.suffix">
+                  , you can try the default accounts:
+                  <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;)
+                  <br />- User (login=&quot;user&quot; and password=&quot;user&quot;).
+                </Translate>
+              </Alert>
 
-            <Alert color="warning">
-              <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
-              <Link to="/account/register" className="alert-link">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </div>
-        )}
-      </Col>
-    </Row>
+              <Alert color="warning">
+                <Translate contentKey="global.messages.info.register.noaccount">You do not have an account yet?</Translate>&nbsp;
+                <Link to="/account/register" className="alert-link">
+                  <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
+                </Link>
+              </Alert>
+            </div>
+          )}
+        </Col>
+      </Row>
     </Fragment>
   );
 };
